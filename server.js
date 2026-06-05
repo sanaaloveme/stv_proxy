980bd8d7bacda2bdfc4a835dc0bff8f35d9913d9
sanaaloveme <sanaaloveme9@gmail.com>
2026-06-05 23:34:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const SECRET_TOKEN = "MY_SECRET_123";
 
 const app = express();
 
@@ -12,17 +13,37 @@ app.get("/", (req, res) => {
     res.send("Proxy Server is Running 🚀");
 });
 
-app.get("/watch/:id", (req, res) => {
+app.get("/watch/:id", async (req, res) => {
 
     const id = req.params.id;
+    const token = req.query.token;
+
+    // 🔐 التحقق من التوكن
+    if (token !== SECRET_TOKEN) {
+        return res.status(403).send("Invalid Token ❌");
+    }
+
     const url = streams[id];
 
     if (!url) {
-        return res.status(404).send("Not found");
+        return res.status(404).send("Stream not found");
     }
 
-    // نرسل المستخدم مباشرة للمصدر
-    res.redirect(url);
+    try {
+        const response = await axios({
+            method: "GET",
+            url: url,
+            responseType: "stream"
+        });
+
+        res.setHeader("Content-Type", "video/mp2t");
+        res.setHeader("Content-Disposition", "inline");
+
+        response.data.pipe(res);
+
+    } catch (err) {
+        res.status(500).send("Error loading stream");
+    }
 });
 
 const PORT = process.env.PORT || 3000;
